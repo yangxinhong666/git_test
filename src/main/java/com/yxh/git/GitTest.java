ddf418ce763ce74ea679a037570c6cc1ad4949b8
yangxinhong <1252500367@qq.com>
2023-03-08 10:57:14 +0800

    second commit1057

--- src/main/java/com/yxh/git/GitTest.java
+++ src/main/java/com/yxh/git/GitTest.java
@@ -12,5 +12,6 @@ package com.yxh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("证明是第二个版本");
     }
 }
