c5bac2aff3c2869735ba64461652860713ec242e
yangxinhong <1252500367@qq.com>
2023-03-08 10:54:05 +0800

    first commit1053

---
+++ src/main/java/com/yxh/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.yxh.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.yxh.git
+ * Description:
+ *
+ * @Author 杨新鸿
+ * @Create 2023/3/8 10:49
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
